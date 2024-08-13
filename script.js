a79dee7e792144bb0457844ff3ef992520a42c7e
Israel <chiagozieohaemesi202@gmail.com>
2024-08-13 15:56:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 19) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('#guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('🚫 No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('Correct! Good guess 👏🎉');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥🤣 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//Resetting the game
+document.querySelector('.reset').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '??';
+  document.querySelector('#guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
